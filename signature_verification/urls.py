27cf1da1b0b8768494795af37ecb1623503d7a5d
Hainid07 <dasariharini07@gmail.com>
2024-11-27 01:02:14 +0530

    Added signature_verification folder

---
+++ signature_verification/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from main import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('upload/', views.upload, name='upload'),
+    path('logout/', views.logout_view, name='logout'),
+]
